4e4cd87e30c346357bb72a6e6020d39e8ed7f7fe
Ryogo Hasumoto <ryogo@RyogonoMacBook-Pro.local>
2023-10-29 17:33:07 +0900

    add #2 動画ファイルを指定したローカルpathに保存する機能追加

--- src/main/java/com/myapp/myvideoapplication/web/rest/PostResource.java
+++ src/main/java/com/myapp/myvideoapplication/web/rest/PostResource.java
@@ -3,8 +3,12 @@ package com.myapp.myvideoapplication.web.rest;
 import com.myapp.myvideoapplication.domain.Post;
 import com.myapp.myvideoapplication.repository.PostRepository;
 import com.myapp.myvideoapplication.web.rest.errors.BadRequestAlertException;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -16,10 +20,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import tech.jhipster.web.util.HeaderUtil;
 import tech.jhipster.web.util.PaginationUtil;
@@ -32,6 +44,8 @@ import tech.jhipster.web.util.ResponseUtil;
 @RequestMapping("/api")
 @Transactional
 public class PostResource {
+
+    private static final String FILE_UPLOAD_PATH = "/var/tmp/myvideoapplication/upload/";
 
     private final Logger log = LoggerFactory.getLogger(PostResource.class);
 
@@ -60,6 +74,18 @@ public class PostResource {
             throw new BadRequestAlertException("A new post cannot already have an ID", ENTITY_NAME, "idexists");
         }
         Post result = postRepository.save(post);
+        byte[] bytes = result.getFile();
+        String fileName = "sample.mp4";
+        //        TODO: fileName追加
+        //        string fileName = result.getFileName();
+        Path path = Paths.get(FILE_UPLOAD_PATH + fileName);
+        try {
+            Files.write(path, bytes);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         return ResponseEntity
             .created(new URI("/api/posts/" + result.getId()))
             .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
